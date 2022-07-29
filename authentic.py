49f4c7c48c361ff32c659b0602d7e124c697d3d8
egalawan <thisuranawalage@gmail.com>
2022-07-28 18:09:46 -0700

    Test1

--- authentic.py
+++ authentic.py
@@ -1,16 +1,31 @@
 def welcome():
     print("Welcome to Hotel Scrummy, the Hotel for all your needs")
-    answer = input("New User or Login(no caps): ")
-    if answer == 'new user':
-        print("hello")
+    answer = input("New User(1) or Login(2): ")
+    if answer == '1':
+        print("Hello New User")
+    else:
+        print("Welcome Back User")
+        UserName2 = input("Please Enter Username: ")
+        with open("HotelRooms.txt", "r") as file:
+         size = 100
+         FileNames = file.read(size)
+         while len(FileNames) > 0:
+            print(FileNames, end = '')
+            FileNames = file.read(size)
+
+with open("HotelRooms.txt", "r") as file:
+         size = 100
+         FileNames = file.read(size)
+         while len(FileNames) > 0:
+            print(FileNames, end = '')
+            FileNames = file.read(size)        
 welcome()
 def register():
-    db = open("HotelRooms.txt", "r")
-    
+    FullName = input("Insert First Name and Last name: ")
     Username = input("Create username: ")
     Password = input ("Create password: ")
     Password2 = input ("Confirm password: ")
-
+    RoomNumber = input ("Which Room would you like: ")
     if Password != Password2:
         print("Passwords dont match, restart")
         register()
@@ -23,9 +38,10 @@ def register():
         #   register()
         else:
             db = open("HotelRooms.txt", "a")
-            db.write(Username + ", " + Password + "\n")
+            db.write(FullName + "     " + Username + "       " + Password + "       " + RoomNumber + "\n")
             print("Success!")
 register()
+print("Thank you for visiting")
 
 def access():
     pass

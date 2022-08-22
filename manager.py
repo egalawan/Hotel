d1e69a6d913d49933aa0f0117799b8a8876e54b9
egalawan <thisuranawalage@gmail.com>
2022-08-22 13:52:06 -0700

    adding manager class with password page for them

--- manager.py
+++ manager.py
@@ -1,17 +1,24 @@
 import pandas as pd
-import csv
 class Manager():
     def show_report(self):
-        print("yo")
-        with open('user_data.csv', 'r') as f:
-            self.reader2 = csv.reader(f)
-            #next(self.reader2)
-            for row in self.reader2:
-                data = " ".join(row)
-            return data 
+        df = pd.read_csv("user_data.csv")
+        df2 = df.to_string(index=False)
+        return df2
 
         ##we can have multiple managers with different passwords if we need?
 
-    def manager():
-        manager = "joe"
-        print(manager)
+    def manager(self,which):
+        self.password = {'6728','8930'}
+        
+        if which not in self.password:
+            return "no password"
+
+        elif which == '6728':
+            manager = "Joe Paskado"
+            return(manager)
+        
+        elif which == '8930':
+            manager = "Sergio Ramiro"
+            return(manager)
+
+        
--- receipt.py
+++ receipt.py
@@ -5,19 +5,22 @@ from email.message import EmailMessage
 
 class Receipt():
     def send_email(self, room, email_receiver,name):
-        #rooms = {"12":0, "42":1, "11":2, "72":3, "69420":4}
-        
-        #df =pd.read_csv("user_data.csv")
+        #email used to send the confirmation to the user
         self.email_sender = "hotelscrummymgmt@gmail.com"
         self.email_password = "exjcwnwmmqqpzrpy"
-
-        self.subject = "Reservation Details"
-        self.body = ("Thank you " + name + " for booking with us, your room number is : " + room)
-
+        #subject of the email
+        self.subject_ = "Reservation Details"
+        #body of the email telling the user their information for their room
+        self.body = ("Thank you " + name + " for reserving at Hotel Scrummy. Your room number is : " + room + "\n" +
+            "We hope you enjoy your room and all of its amenities. ." + 
+            "Please reply to this email with any questions or concerns. We welcome you back at any time \n" + 
+            "Stay scrummy,\n"+
+            "Mr. Scrummy of Hotel Scrummy")    
+            
         self.em = EmailMessage()
         self.em["From"] = self.email_sender
         self.em["To"] = email_receiver
-        self.em["Subject"] = self.subject
+        self.em["Subject"] = self.subject_
         self.em.set_content(self.body)
 
         context = ssl.create_default_context()
